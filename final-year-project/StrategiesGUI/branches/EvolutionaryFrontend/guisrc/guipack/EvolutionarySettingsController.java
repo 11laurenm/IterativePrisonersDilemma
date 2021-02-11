1f99fb3c16b0cd5c75459238788e8f07c0b59716
zfac043 <zfac043@31da6ae9-1b57-47bb-a73d-c0b666ba6535>
2021-02-11 15:52:11 +0000

    Removed code for grid in evolutionary tournament in order to try constructing graphs instead.

--- final-year-project/StrategiesGUI/branches/EvolutionaryFrontend/guisrc/guipack/EvolutionarySettingsController.java
+++ final-year-project/StrategiesGUI/branches/EvolutionaryFrontend/guisrc/guipack/EvolutionarySettingsController.java
@@ -1,6 +1,5 @@
 package guipack;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -9,21 +8,12 @@ import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.control.RadioButton;
-import javafx.scene.control.Slider;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.control.TableColumn.CellDataFeatures;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.ColumnConstraints;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.RowConstraints;
 import javafx.util.Callback;
 import strategiespack.Strategy;
 
@@ -63,29 +53,14 @@ public class EvolutionarySettingsController {
   
   private ObservableList<Strategy> stratsList = FXCollections.observableArrayList();
   
-  @FXML
-  private BorderPane graphPane;
-  
-  private String toLoad;
-  
-  @FXML
-  private GridPane gridGraph;
-  
-  @FXML
-  private TextField rows;
-  
-  @FXML
-  private TextField cols;
-  
   public EvolutionarySettingsController() {
     
   }
   
   @FXML 
   public void initialize(Main main, ArrayList<Strategy> strats) {
-    setBorderPane();
     setMain(main);
-    gridResize();
+
     stratsList = FXCollections.observableArrayList(strats);
     stratsTable.setItems(stratsList);
     
@@ -111,68 +86,6 @@ public class EvolutionarySettingsController {
   @FXML
   public void backButton() {
     mainn.showOverview();
-  }
-  
-  @FXML
-  public void setBorderPane() {
-    if(gridButton.isSelected()) {
-      toLoad = "Grid";
-    } else if (circleGraphButton.isSelected()) {
-      toLoad = "Circle";
-    } else if (starGraphButton.isSelected()) {
-      toLoad = "Star";
-    } else if (completeGraphButton.isSelected()) {
-      toLoad = "Complete";
-    } else if (bipartiteGraphButton.isSelected()) {
-      toLoad = "Bipartite";
-    } else if (busGraphButton.isSelected()) {
-      toLoad = "Bus";
-    }
-    
-    FXMLLoader loader = new FXMLLoader(getClass().getResource(toLoad + "Graph.fxml"));
-    loader.setController(this);
-    try {
-      graphPane.setCenter(loader.load());
-    } catch (IOException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-    }
-  }
-  
-  @FXML
-  public void gridSquareSelected() {
-    
-  }
-  
-  @FXML
-  public void gridResize() {
-    int totalRows = Integer.parseInt(rows.getText());
-    int totalCols = Integer.parseInt(cols.getText());
-    int currentRows = gridGraph.getRowConstraints().size();
-    int currentCols = gridGraph.getColumnConstraints().size();
-    
-    if(totalRows < currentRows) {
-      for(int i = 0; i < currentRows - totalRows; i++) {
-        gridGraph.getRowConstraints().remove(currentRows - 1);
-      }
-    } else if(totalCols < currentCols) {
-      for(int i = 0; i < currentCols - totalCols; i++) {
-        gridGraph.getColumnConstraints().remove(currentCols - 1);
-      }
-    } else if(totalRows > currentRows) {
-      for(int i = 0; i < totalRows - currentRows; i++) {
-        RowConstraints row1 = new RowConstraints();
-        row1.setPercentHeight(100 / (totalRows));
-        gridGraph.getRowConstraints().add(row1);
-      }
-    } else {
-      for(int i = 0; i < totalCols - currentCols; i++) {
-        ColumnConstraints col1 = new ColumnConstraints();
-        col1.setPercentWidth(100 / (totalCols));
-        gridGraph.getColumnConstraints().add(col1);
-      }
-    }
-    
   }
 
 }

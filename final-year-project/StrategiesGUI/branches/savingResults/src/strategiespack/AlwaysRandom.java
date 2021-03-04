9a4097224925749de60222e32870f40410f6e5e4
zfac043 <zfac043@31da6ae9-1b57-47bb-a73d-c0b666ba6535>
2021-03-04 23:26:40 +0000

    Created branch that will contain code regarding the saving/exporting of results

---
+++ final-year-project/StrategiesGUI/branches/savingResults/src/strategiespack/AlwaysRandom.java
@@ -0,0 +1,80 @@
+package strategiespack;
+
+import java.util.ArrayList;
+import java.util.Random;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+/** A strategy that always makes a random decision.
+ * 
+ * @author Lauren Moore -zfac043
+ *
+ */
+
+public class AlwaysRandom extends Strategy {
+
+  /**
+   * A random number generator used to generate a random boolean.
+   */
+  Random booleanGenerator = new Random();
+    
+  /**
+   * Constructor for an instance of the strategy, needs no inputs.
+  */
+  public AlwaysRandom() {
+    
+  }
+  
+  /**
+   * returns the decision made by the strategy for the turn in which it is called.
+   * @param lastMove - the last move made by its opponent
+   * @param myHistory - all previous moves made by the strategy
+   * @param myLastMove - last move made by the strategy
+   * @param opponentPoints - score of opponent
+   * @return the character c for cooperate if the random bool is true, otherwise return d
+  */
+  @Override
+  public char getDecision(char lastMove, ArrayList<Character> opponentHistory, 
+          ArrayList<Character> myHistory, char myLastMove, int opponentPoints) {
+    if (booleanGenerator.nextBoolean()) {
+      return 'c';
+    }
+    return 'd';
+  }
+  
+  /**
+   * Method that provides the strategy's name.
+   * @return the name of the strategy as a SimpleStringProperty
+   */
+  public StringProperty nameProperty() {
+    SimpleStringProperty strategyName = new SimpleStringProperty();
+    strategyName.setValue("AlwaysRandom");
+    return strategyName;
+  }
+  
+  /**
+   * Method that provides the strategy's probability (if one exists).
+   * @return the value of the probability variable of the strategy as a SimpleStringProperty
+   */
+  public StringProperty probabilityProperty() {
+    SimpleStringProperty prob = new SimpleStringProperty();
+    prob.setValue("0");
+    return prob;
+  }
+  
+  /**
+   * Method that provides the number of rounds the strategy considers (if one exists).
+   * @return the value of the rounds variable of the strategy as a SimpleStringProperty
+   */
+  public StringProperty roundsProperty() {
+    SimpleStringProperty rounds = new SimpleStringProperty();
+    rounds.setValue("0");
+    return rounds;
+  }
+  
+  public StringProperty colourProperty() {
+    SimpleStringProperty colour = new SimpleStringProperty();
+    colour.setValue("Blue");
+    return colour;
+  }
+}
